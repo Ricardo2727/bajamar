ae00176a0ead48f1e7b46821c202f326ad62d8a0
Ricardo2727 <valenzuelaopazor@gmail.com>
2024-04-02 17:45:47 -0300

    proyecto final v11

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import  { getFirestore} from 'firebase/firestore'
 
 
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAOhA21TuK4YM_TmJaRsxtGwr7afVRam-0",
   authDomain: "backend-bajamar.firebaseapp.com",
