4ff7bc11b3c6657c9d5fe84adada78d1477492ba
Ricardo2727 <valenzuelaopazor@gmail.com>
2024-04-02 17:57:24 -0300

    proyecto final v14

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -1,9 +1,26 @@
 
+// import { initializeApp } from "firebase/app";
+// //import  { getFirestore} from 'firebase/firestore'
+// import  { getFirestore} from 'firebase/firebaseConfig'
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAOhA21TuK4YM_TmJaRsxtGwr7afVRam-0",
+//   authDomain: "backend-bajamar.firebaseapp.com",
+//   projectId: "backend-bajamar",
+//   storageBucket: "backend-bajamar.appspot.com",
+//   messagingSenderId: "235851420527",
+//   appId: "1:235851420527:web:df97b550dfc68b9b63ce9a"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export const db = getFirestore(app)
+
 import { initializeApp } from "firebase/app";
-//import  { getFirestore} from 'firebase/firestore'
-import  { getFirestore} from 'firebase/firebaseConfig'
-
-
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAOhA21TuK4YM_TmJaRsxtGwr7afVRam-0",
@@ -14,7 +31,7 @@ const firebaseConfig = {
   appId: "1:235851420527:web:df97b550dfc68b9b63ce9a"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-export const db = getFirestore(app)
+export { db };
